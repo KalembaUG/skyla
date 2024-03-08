aff1d8e8ef70fd6d13f33eccc70e9f959d6d54b8
KalembaUG <kalembajamil01@gmail.com>
2024-03-08 16:48:09 +0300

    failed

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,8 +3,11 @@
 set -o errexit
 
 bundle install
-bundle exec rake assets:precompile
+# clean
+rm -rf public
+# build
+npm install --prefix client && npm run build --prefix client
+# migrate
 bundle exec rake db:migrate
-
-bundle exec rake db:seed
-bundle exec rake assets:clean;
+# postbuild
+cp -a client/build/. public/
